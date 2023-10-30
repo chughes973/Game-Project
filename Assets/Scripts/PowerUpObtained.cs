a890ec5b935d19586629d4e00c00f90f680f8215
chughes973 <146023719+chughes973@users.noreply.github.com>
2023-10-30 20:44:49 +0000

    Eating sound when powerup obtained

--- Assets/Scripts/PowerUpObtained.cs
+++ Assets/Scripts/PowerUpObtained.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class PowerUpObtained : MonoBehaviour
 {
     public GameObject prefabToDestroy; //assign the prefab to destroy, in this case it's the powerup
+    public AudioSource playerAudio;
+    public AudioClip powerupConsume;
 
 
     void OnTriggerEnter(Collider other) //destroys obstacles when collided with projectile.
@@ -13,7 +15,9 @@ public class PowerUpObtained : MonoBehaviour
         if (other.CompareTag("PowerUp"))
         {
             Destroy(other.gameObject);
+            playerAudio.PlayOneShot(powerupConsume, 1.0f);
             ScoreManager.totalScore += 5; //score is increased by 1 and game object is destroyed  
+            
         }
     }
 
