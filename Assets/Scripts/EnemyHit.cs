d208f7979b86a24e79ce28290e30e94c1a98de42
chughes973 <146023719+chughes973@users.noreply.github.com>
2023-10-31 16:57:49 +0000

    EnemyAudio
    Not Working

--- Assets/Scripts/EnemyHit.cs
+++ Assets/Scripts/EnemyHit.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class EnemyHit : MonoBehaviour
 {
+    public AudioSource playerAudio;
+    public AudioClip enemyKilled;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -23,7 +25,9 @@ public class EnemyHit : MonoBehaviour
 
             Destroy(gameObject); //destroys projectile
             Destroy(other.gameObject);//destroys enemy when hit - need to change to be death animation and then destroy in next iteration
+            playerAudio.PlayOneShot(enemyKilled, 1.0f); //shot impact sound plays when projectile interacts with enemy
             ScoreManager.totalScore += 10; //score is increased by 1 and game object is destroyed  
+
         }
     }
 }
